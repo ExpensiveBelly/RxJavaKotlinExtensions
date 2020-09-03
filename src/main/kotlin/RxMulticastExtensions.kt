4c501981c23dd4a0285158211eca84212fb90c0c
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2020-09-03 21:36:57 +0100

    Single will only emit one item so no need for `bufferSize`

--- src/main/kotlin/RxMulticastExtensions.kt
+++ src/main/kotlin/RxMulticastExtensions.kt
@@ -9,8 +9,8 @@ import java.util.concurrent.TimeUnit
 fun <T> Observable<T>.broadcast(bufferSize: Int = 1, duration: Duration = Duration.ZERO): Observable<T> =
     replay(bufferSize).refCount(duration.toNanos(), TimeUnit.NANOSECONDS)
 
-fun <T> Single<T>.broadcast(bufferSize: Int = 1, duration: Duration = Duration.ZERO): Single<T> =
-    toObservable().replay(bufferSize).refCount(duration.toNanos(), TimeUnit.NANOSECONDS).firstOrError()
+fun <T> Single<T>.broadcast(duration: Duration = Duration.ZERO): Single<T> =
+    toObservable().replay(1).refCount(duration.toNanos(), TimeUnit.NANOSECONDS).firstOrError()
 
 fun <T> Single<T>.share(): Single<T> = toObservable().share().firstOrError()
 fun <T> Maybe<T>.share(): Maybe<T> = toObservable().share().firstElement()
