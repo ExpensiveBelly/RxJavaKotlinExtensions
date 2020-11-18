0a2b23ac1d7ebb669a313b9de80c3bee688f484e
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2020-11-18 20:25:49 +0000

    Update RxLoggingExtensions.kt

--- src/main/kotlin/RxLoggingExtensions.kt
+++ src/main/kotlin/RxLoggingExtensions.kt
@@ -6,63 +6,58 @@ import io.reactivex.rxjava3.core.*
  * https://gist.github.com/tomaszpolanski/99c37c388e06e57ef72a5c8e752b8c2c
  */
 
-inline fun <reified T> printEvent(tag: String, success: T?, error: Throwable?) =
-    when {
-        success == null && error == null -> RxLog.d(tag, "Complete") /* Only with Maybe */
-        success != null -> RxLog.d(tag, "Success $success")
-        error != null -> RxLog.d(tag, "Error $error")
-        else -> -1 /* Cannot happen*/
-    }
+inline fun <reified T> printEvent(tag: String, success: T?, error: Throwable?): Any =
+	when {
+		success == null && error == null -> Log.d(tag, "Complete") /* Only with Maybe */
+		success != null -> Log.d(tag, "Success $success")
+		error != null -> Log.d(tag, "Error $error")
+		else -> -1 /* Cannot happen*/
+	}
 
 inline fun printEvent(tag: String, error: Throwable?) =
-    when {
-        error != null -> RxLog.d(tag, "Error $error")
-        else -> RxLog.d(tag, "Complete")
-    }
+	when {
+		error != null -> Log.d(tag, "Error $error")
+		else -> Log.d(tag, "Complete")
+	}
 
 /**
  * Example usage of [log]:
- *   Single.timer(1, TimeUnit.SECONDS).log().subscribe ({ }, { })
+ *   Single.timer(1, TimeUnit.SECONDS).log().subscribe({ }, { })
  */
 
 inline fun tag() =
-    Thread.currentThread().stackTrace
-        .first { it.fileName.endsWith(".kt") }
-        .let { stack -> "${stack.fileName.removeSuffix(".kt")}::${stack.methodName}:${stack.lineNumber}" }
+	Thread.currentThread().stackTrace
+		.first { it.fileName?.endsWith(".kt") ?: false }
+		.let { stack -> "${stack?.fileName?.removeSuffix(".kt")}::${stack.methodName}:${stack.lineNumber}" }
 
-inline fun <reified T> Single<T>.log(tag: String = tag()): Single<T> {
-    return doOnEvent { success, error -> printEvent(tag, success, error) }
-        .doOnSubscribe { RxLog.d(tag, "Subscribe") }
-        .doOnDispose { RxLog.d(tag, "Dispose") }
+inline fun <reified T> Single<T>.log(tag: String = tag()) =
+	doOnEvent { success, error -> printEvent(tag, success, error) }
+		.doOnSubscribe { Log.d(tag, "Subscribe") }
+		.doOnDispose { Log.d(tag, "Dispose") }
+
+inline fun <reified T> Maybe<T>.log(tag: String = tag()) =
+	doOnEvent { success, error -> printEvent(tag, success, error) }
+		.doOnSubscribe { Log.d(tag, "Subscribe") }
+		.doOnDispose { Log.d(tag, "Dispose") }
+
+inline fun Completable.log(tag: String = tag()) = doOnEvent { printEvent(tag, it) }
+	.doOnSubscribe { Log.d(tag, "Subscribe") }
+	.doOnDispose { Log.d(tag, "Dispose") }
+
+inline fun <reified T> Observable<T>.log(tag: String = tag()) = doOnEach { Log.d(tag, "Each $it") }
+	.doOnSubscribe { Log.d(tag, "Subscribe") }
+	.doOnDispose { Log.d(tag, "Dispose") }
+
+inline fun <reified T> Flowable<T>.log(tag: String = tag()) = doOnEach { Log.d(tag, "Each $it") }
+	.doOnSubscribe { Log.d(tag, "Subscribe") }
+	.doOnCancel { Log.d(tag, "Cancel") }
+
+
+object Log {
+	fun d(tag: String, message: String) {
+		println("$tag : $currentThread : $message")
+	}
 }
 
-inline fun <reified T> Maybe<T>.log(tag: String = tag()): Maybe<T> {
-    return doOnEvent { success, error -> printEvent(tag, success, error) }
-        .doOnSubscribe { RxLog.d(tag, "Subscribe") }
-        .doOnDispose { RxLog.d(tag, "Dispose") }
-}
-
-inline fun Completable.log(tag: String = tag()): Completable {
-    return doOnEvent { printEvent(tag, it) }
-        .doOnSubscribe { RxLog.d(tag, "Subscribe") }
-        .doOnDispose { RxLog.d(tag, "Dispose") }
-}
-
-inline fun <reified T> Observable<T>.log(tag: String = tag()): Observable<T> {
-    return doOnEach { RxLog.d(tag, "Each $it") }
-        .doOnSubscribe { RxLog.d(tag, "Subscribe") }
-        .doOnDispose { RxLog.d(tag, "Dispose") }
-}
-
-inline fun <reified T> Flowable<T>.log(tag: String = tag()): Flowable<T> {
-    return doOnEach { RxLog.d(tag, "Each $it") }
-        .doOnSubscribe { RxLog.d(tag, "Subscribe") }
-        .doOnCancel { RxLog.d(tag, "Cancel") }
-}
-
-
-object RxLog {
-    fun d(tag: String, message: String) {
-        println("$tag : $message")
-    }
-}
+private val currentThread
+	get() = Thread.currentThread().name
